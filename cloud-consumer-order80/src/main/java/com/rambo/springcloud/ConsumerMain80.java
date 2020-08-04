64df06322382479ca70552f52cfa30eac3e13c56
rambo <Rambo.zhai@cardinfolink.com>
2020-08-04 12:30:21 +0800

    'order'

--- cloud-consumer-order80/src/main/java/com/rambo/springcloud/ConsumerMain80.java
+++ cloud-consumer-order80/src/main/java/com/rambo/springcloud/ConsumerMain80.java
@@ -1,10 +1,15 @@
 package com.rambo.springcloud;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by 翟博文 on 2020/8/3 23:11
  */
+@SpringBootApplication
 public class ConsumerMain80 {
     public static void main(String[] args) {
-
+        SpringApplication.run(ConsumerMain80.class);
     }
 }
